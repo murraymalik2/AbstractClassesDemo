7bb2452586d9c3db57b78606f0129416012e2899
murra <murraymalik2@gmail.com>
2023-07-21 16:07:16 -0400

    Console UI

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,6 +13,54 @@ namespace ConsoleUI
             /*
              * Todo follow all comments!! 
              */
+            var vehicles = new List<Vehicle>();
+
+            var car1 = new Car()
+            { 
+                HasTrunk = true,
+                Year = "2000",
+                Make = "Honda",
+                Model = "Accord"
+            };
+            var motorcycle1 = new Motorcycle() 
+            { 
+                HasSideCart = true,
+                Year = "2001",
+                Make = "Kawasaki ",
+                Model = "Ninja"
+            };
+
+            Vehicle vehicle1 = new Car() 
+            {
+                Year = "2020",
+                Make = "Tesla ",
+                Model = "X"
+            };
+            Vehicle vehicle2 = new Motorcycle() 
+            {
+                Year = "2010",
+                Make = "Suzuki",
+                Model = "GSXR"
+            };
+
+            vehicles.Add(car1);
+            vehicles.Add(motorcycle1);
+            vehicles.Add(vehicle1);
+            vehicles.Add(vehicle2);
+            
+            foreach (var vehicle in vehicles) 
+            { 
+                Console.WriteLine($" Year:{vehicle.Year}");
+                Console.WriteLine($"Make:{ vehicle.Make}");
+                Console.WriteLine($" Model: {vehicle.Model}");
+                Console.WriteLine();
+                vehicle.DriveVirtual();
+                vehicle.DriveAbstract();
+                Console.WriteLine();
+            }
+
+           
+
 
             #region Vehicles
 
